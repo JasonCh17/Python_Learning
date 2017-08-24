cb9b662c1ba35464522287f849cd4430eec1fd26
JasonCh17 <695534569@qq.com>
2017-08-24 08:37:58 +0800

    2017.8.24

--- Morvan python test.py
+++ Morvan python test.py
@@ -81,11 +81,37 @@
 # print(calcul.price)
 
 
-a_input=input('please give a number:')
-print('This input number is:',a_input)
-if a_input =='1':
-    print('this is a good one')
-elif a_input==str(2):
-    print('see you next time')
-else:
-    print('good luck')
+# a_input=input('please give a number:')
+# print('This input number is:',a_input)
+# if a_input =='1':
+#     print('this is a good one')
+# elif a_input==str(2):
+#     print('see you next time')
+# else:
+#     print('good luck')
+import matplotlib.pyplot as plt
+import numpy as np
+x =np.linspace(-3,3,50)
+y1=2*x +1
+y2=x**2
+
+plt.figure(num=1)
+plt.plot(x,y1)
+
+plt.figure(num=8,figsize=(8,5))
+plt.plot(x,y2)
+plt.plot(x,y1,color='red',linewidth=1.0,linestyle='--')
+
+plt.xlim((-1,2))
+plt.ylim((-2,3))
+plt.xlabel('i am x')
+plt.ylabel('i am y')
+
+new_ticks=np.linspace(-1,2,5)
+print(new_ticks)
+
+plt.xticks(new_ticks)
+plt.yticks([-2,-1.8,-1,1.22,3],
+        [r'$really\ bad $',r'$bad\ \alpha$',r'$normal$',r'$good$','$really\ good$'])
+
+plt.show()
